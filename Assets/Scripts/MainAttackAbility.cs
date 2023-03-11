ef27fb1971c9e30eb0f7985d9b07ba9aa5e66385
Kaloyan Sarafov <kaloyan@sarafov.email>
2023-03-11 03:25:46 +0200

    added black hole to level and main attack ability

---
+++ Assets/Scripts/MainAttackAbility.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace StarterAssets
+{
+    [CreateAssetMenu]
+    public class MainAttackAbility : Ability
+    {
+        
+        public override void Use(GameObject player)
+        {
+            base.Use(player);
+            RobotController controller = player.GetComponent<RobotController>();
+            //controller.MeleeAttack();
+        }
+    }
+}
