9372b19f2212ec87d4ee4d1d4bba0c7924657cee
Blagovest-Hristov <bobo4e.h@gmail.com>
2023-03-11 07:26:09 +0200

    gore dolu raboti ma ne prilicha na nisshto

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -19,8 +19,10 @@ namespace StarterAssets
             //set the player's color
             var meshRenderer = GetComponentInChildren<MeshRenderer>();
             meshRenderer.material.color = Random.ColorHSV();
-        }
         
+
+        }
+
         // Update is called once per frame
         void FixedUpdate()
         {
@@ -70,19 +72,27 @@ namespace StarterAssets
 
         public void Attack()
         {
+          
+            Debug.Log("left click");
+            if (Physics.OverlapSphere(new Vector3(transform.right.x + transform.position.x, transform.position.y + transform.right.y, transform.position.z + transform.right.z), 2f, LayerMask.GetMask("Ball")).Length > 0)
+            {
+                Physics.OverlapSphere(transform.position, 10f, LayerMask.GetMask("Ball"))[0].gameObject
+                    .GetComponent<BallController>().recieveHit(new Vector3(transform.forward.z, transform.forward.y, -transform.forward.x));
 
-            // if left click is hit
-           
-                Debug.Log("left click");
-                if (Physics.OverlapSphere(transform.position, 10, LayerMask.GetMask("Ball")).Length > 0)
-                {
-                    Physics.OverlapSphere(transform.position, 10, LayerMask.GetMask("Ball"))[0].gameObject
-                        .GetComponent<BallController>().recieveHit(transform.forward);
-                    Debug.Log("hit");
-                }
+
+                Debug.Log("hit");
+
+            }
+            
             
 
 
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(new Vector3(transform.right.x + transform.position.x , transform.position.y + transform.right.y, transform.position.z + transform.right.z), 2f);
         }
     }
 }
